8ecf1fa8ac5f9b83f33fcb5426205bf68432a54e
Serra KIR DWORCZAK <153081212+SerraKD@users.noreply.github.com>
2025-02-12 20:12:19 +0000

    build views

--- steelbusiness/main/views.py
+++ steelbusiness/main/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'main/home.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def services(request):
+    return render(request, 'main/services.html')
+
+
+def staircase(request):
+    return render(request, 'main/staircase.html')
+
+
+def gallery(request):
+    return render(request, 'main/gallery.html')
+
+
+def faq(request):
+    return render(request, 'main/faq.html')
+
+
+def contact(request):
+    return render(request, 'main/contact.html')
