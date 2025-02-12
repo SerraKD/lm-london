98fa6eddf6fb9549d448853436f8c5551a7278fa
Serra KIR DWORCZAK <153081212+SerraKD@users.noreply.github.com>
2025-02-12 23:02:03 +0000

    migrate and collect static

--- steelbusiness/steelbusiness/settings.py
+++ steelbusiness/steelbusiness/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-eu-70fzkypub@iz^7ey-x09kla=v^bw$&mo+p_2t!zbg6_7sfb"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
