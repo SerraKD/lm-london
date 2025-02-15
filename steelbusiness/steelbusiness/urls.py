82bccf2ceae79f3b469e8da90b9fdbdaec588ba2
Serra KIR DWORCZAK <153081212+SerraKD@users.noreply.github.com>
2025-02-15 19:18:23 +0000

    remove translate urls

--- steelbusiness/steelbusiness/urls.py
+++ steelbusiness/steelbusiness/urls.py
@@ -23,7 +23,6 @@ from main import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('steelbusiness.urls')),
-    path("i18n/", include("django.conf.urls.i18n"), name="set_language"),
 ]
 
 
